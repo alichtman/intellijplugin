349df740b67d70a224bc74a30327d36197969405
Geoffrey Challen <challen@illinois.edu>
2018-08-30 07:37:39 -0500

    More cleanup and counters working.

--- src/main/kotlin/CS125Component.kt
+++ src/main/kotlin/CS125Component.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.*
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
@@ -39,6 +40,7 @@ class CS125Component :
     data class Counter(
             var index: Long = 0,
             var MP: String = "",
+            var email: String = "",
             var start: Long = Instant.now().toEpochMilli(),
             var end: Long = 0,
             var keystrokeCount: Int = 0,
@@ -69,6 +71,9 @@ class CS125Component :
 
         val connection = ApplicationManager.getApplication().messageBus.connect()
         connection.subscribe(ProjectManager.TOPIC, this)
+
+        val state = CS125Persistence.getInstance().persistentState
+        log.info("Loading " + state.savedCounters.size.toString() + " counters")
 
         ApplicationManager.getApplication().invokeLater {
             EditorFactory.getInstance().eventMulticaster.addCaretListener(this)
@@ -112,7 +117,6 @@ class CS125Component :
 
         val uploadCounterTask = object: Task.Backgroundable(project,"Uploading...", false) {
             override fun run(progressIndicator: ProgressIndicator) {
-                updateEmails()
                 try {
                     Thread.sleep(1000)
                 } catch (e: Exception) { }
@@ -131,7 +135,7 @@ class CS125Component :
 
         val state = CS125Persistence.getInstance().persistentState
 
-        log.info(state.savedCounters.size.toString())
+        log.info("Saved counter count: " + state.savedCounters.size.toString())
 
         val end = Instant.now().toEpochMilli()
         for ((project, counter) in currentProjectCounters) {
@@ -139,10 +143,14 @@ class CS125Component :
                 continue
             }
             counter.end = end
-            log.info(counter.toString())
+            log.info("Counter " + counter.toString())
             state.savedCounters.add(counter)
-            currentProjectCounters[project] = Counter(state.counterIndex++, projectInfo[project]!!.MP)
-            uploadCounters(project)
+            currentProjectCounters[project] = Counter(
+                    state.counterIndex++,
+                    projectInfo[project]?.MP ?: "",
+                    projectInfo[project]?.email ?: ""
+            )
+            //uploadCounters(project)
         }
     }
 
@@ -160,11 +168,17 @@ class CS125Component :
 
         @Suppress("UNCHECKED_CAST")
         val gradeConfiguration = Yaml().load(Files.newBufferedReader(gradeConfigurationFile.toPath())) as Map<String, String>
-        val MPname = gradeConfiguration["name"] ?: return
+        val name = gradeConfiguration["name"] ?: return
+
+        val emailFile = File(project.baseDir.path).resolve(File("email.txt"))
+        var email = ""
+        if (emailFile.exists()) {
+            email = emailFile.readText().trim()
+        }
 
         val state = CS125Persistence.getInstance().persistentState
-        projectInfo[project] = ProjectInfo(MPname)
-        currentProjectCounters[project] = Counter(state.counterIndex++, MPname)
+        projectInfo[project] = ProjectInfo(name)
+        currentProjectCounters[project] = Counter(state.counterIndex++, name, email)
 
         if (currentProjectCounters.size == 1) {
             stateTimer?.cancel()
@@ -188,24 +202,28 @@ class CS125Component :
     }
 
     override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
+        val projectCounter = currentProjectCounters[project] ?: return Result.CONTINUE
         log.info("charTyped")
-        currentProjectCounters[project]!!.keystrokeCount++
+        projectCounter.keystrokeCount++
         return Result.CONTINUE
     }
 
     override fun caretPositionChanged(caretEvent: CaretEvent) {
+        val projectCounter = currentProjectCounters[caretEvent.editor.project] ?: return
         log.info("caretPositionChanged")
-        currentProjectCounters[caretEvent.editor.project]!!.caretPositionChangedCount++
+        projectCounter.caretPositionChangedCount++
     }
 
     override fun visibleAreaChanged(visibleAreaEvent: VisibleAreaEvent) {
+        val projectCounter = currentProjectCounters[visibleAreaEvent.editor.project] ?: return
         log.info("visibleAreaChanged")
-        currentProjectCounters[visibleAreaEvent.editor.project]!!.visibleAreaChangedCount++
+        projectCounter.visibleAreaChangedCount++
     }
 
     override fun mousePressed(editorMouseEvent: EditorMouseEvent) {
+        val projectCounter = currentProjectCounters[editorMouseEvent.editor.project] ?: return
         log.info("mousePressed")
-        currentProjectCounters[editorMouseEvent.editor.project]!!.mousePressedCount++
+        projectCounter.mousePressedCount++
     }
     override fun mouseClicked(e: EditorMouseEvent) {}
     override fun mouseReleased(e: EditorMouseEvent) {}
@@ -213,29 +231,27 @@ class CS125Component :
     override fun mouseExited(e: EditorMouseEvent) {}
 
     override fun selectionChanged(selectionEvent: SelectionEvent) {
+        val projectCounter = currentProjectCounters[selectionEvent.editor.project] ?: return
         log.info("selectionChanged")
-        currentProjectCounters[selectionEvent.editor.project]!!.selectionChangedCount++
+        projectCounter.selectionChangedCount++
     }
 
     override fun documentChanged(documentEvent: DocumentEvent) {
         log.info("documentChanged")
-        val editors = EditorFactory.getInstance().getEditors(documentEvent.document)
-        if (editors.isEmpty()) {
-            return
-        }
-        editors.forEach { editor ->
-            currentProjectCounters[editor.project]!!.documentChangedCount++
-        }
-    }
 
-    private fun updateEmails() {
-        for ((project, projectInfo) in projectInfo) {
-            val emailFile = File(project.baseDir.path).resolve(File("email.txt"))
-            if (!(emailFile.exists())) {
-                projectInfo.email = ""
-            } else {
-                projectInfo.email = emailFile.readText().trim()
+        val changedFile = FileDocumentManager.getInstance().getFile(documentEvent.document)
+        for ((project, info) in projectInfo) {
+            val emailPath = File(project.baseDir.path).resolve(File("email.txt")).canonicalPath
+            if (changedFile?.canonicalPath.equals(emailPath)) {
+                info.email = documentEvent.document.text.trim()
+                log.info("Updated email for project " + info.MP + ": " + info.email)
             }
+        }
+
+        val editors = EditorFactory.getInstance().getEditors(documentEvent.document)
+        for (editor in editors) {
+            val projectCounter = currentProjectCounters[editor.project] ?: continue
+            projectCounter.documentChangedCount++
         }
     }
 }
